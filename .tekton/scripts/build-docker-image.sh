999449d5e77247f08adf5bd4564cdf76a02ffe50
AFAvella <aavella62419@universidadean.edu.co>
2023-07-12 15:02:44 -0500

    Prueba 26

--- .tekton/scripts/build-docker-image.sh
+++ .tekton/scripts/build-docker-image.sh
@@ -85,11 +85,16 @@ else
   done
 fi
 set -x
-buildctl build \
-    --frontend dockerfile.v0 --opt filename=${DOCKER_FILE} --local dockerfile=${DOCKER_ROOT} \
-    ${BUILD_ARGS} --local context=${DOCKER_ROOT} \
-    --import-cache type=registry,ref=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME} \
-    --output type=image,name="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}",push=true
+buildctl --addr tcp://0.0.0.0:1234 build \
+  --progress=plain \
+  --frontend=dockerfile.v0 \
+  --opt filename=$(params.dockerfile) \
+  ${BUILD_ARGS} \
+  --local context=$(workspaces.task-pvc.path)/$(params.path-to-context) \
+  --local dockerfile=$(workspaces.task-pvc.path)/$(params.path-to-dockerfile) \
+  --exporter=image --exporter-opt "name=$BUILDKIT_IMAGE_NAMES" --exporter-opt "push=$(params.push-to-registry)" \
+  --export-cache type=inline \
+  --import-cache type=registry,ref=$IMAGE_REPOSITORY 2>&1 | tee /steps/build.log
 set +x
 docker buildx create --config /home/rami/workspace/gaia/buildkitd.toml  --name myconfbuilder4
 docker buildx use myconfbuilder4
@@ -132,64 +137,3 @@ echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}" >> $ARCHIVE_DIR/build.properties
 echo "GIT_BRANCH=${GIT_BRANCH}" >> $ARCHIVE_DIR/build.properties
 echo "File 'build.properties' created for passing env variables to subsequent pipeline jobs:"
 cat $ARCHIVE_DIR/build.properties | grep -v -i password
-
-if [ $PIPELINE_DEBUG == 1 ]; then
-  pwd
-  env
-  trap env EXIT
-  set -x
-fi
-
-source /steps/next-step-env.properties
-export $(cut -d= -f1 /steps/next-step-env.properties)
-
-# Manage multiple tags for an image
-# Add dynamically computed tags
-printf "#!/bin/sh\n" > /steps/additionalTags.sh
-printf "%s " '$(params.additional-tags-script)' >> /steps/additionalTags.sh
-chmod +x /steps/additionalTags.sh
-
-# Send stdout to the tags list; don't touch stderr.
-/steps/additionalTags.sh > /steps/tags.lst
-
-# Add image pipeline resource
-if [ "${IMAGE_TAG}" ]; then
-  echo "${IMAGE_TAG}" >> /steps/tags.lst
-fi
-# Add tags provided using task parameter
-if [ "$(params.additional-tags)" ];  then
-  echo "$(params.additional-tags)" | sed 's/,/\n/g' >> /steps/tags.lst
-fi
-echo "#######################"
-echo "Image Tags:"
-cat /steps/tags.lst
-echo "#######################"
-IMAGE_REPOSITORY="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
-# Add the full image url with tags - use # as separator in case IMAGE_NAME contains /
-sed -i "s#^#$IMAGE_REPOSITORY:#" /steps/tags.lst
-sort -u -o /steps/tags.lst /steps/tags.lst
-echo "Full Image URLs:"
-cat /steps/tags.lst
-echo "#######################"
-BUILDKIT_IMAGE_NAMES=$(tr -s '\r\n' ',' < /steps/tags.lst | sed -e 's/,$/\n/')
-if [ -z "$BUILDKIT_IMAGE_NAMES" ]; then
-# Set default image name for buildkit to push
-BUILDKIT_IMAGE_NAMES="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
-fi
-echo "Buildkit Image names: $BUILDKIT_IMAGE_NAMES"
-
-BUILD_ARG_LIST='$(params.build-args)'
-for buildArg in $BUILD_ARG_LIST; do
-BUILD_ARGS="${BUILD_ARGS} --opt build-arg:$buildArg "
-done
-
-buildctl --addr tcp://0.0.0.0:1234 build \
---progress=plain \
---frontend=dockerfile.v0 \
---opt filename=$(params.dockerfile) \
-${BUILD_ARGS} \
---local context=$(workspaces.task-pvc.path)/$(params.path-to-context) \
---local dockerfile=$(workspaces.task-pvc.path)/$(params.path-to-dockerfile) \
---exporter=image --exporter-opt "name=$BUILDKIT_IMAGE_NAMES" --exporter-opt "push=$(params.push-to-registry)" \
---export-cache type=inline \
---import-cache type=registry,ref=$IMAGE_REPOSITORY 2>&1 | tee /steps/build.log
