5a355e2b2c2ba12f55ddb7a2820f2a65238851bf
AFAvella <aavella62419@universidadean.edu.co>
2023-07-12 13:47:59 -0500

    Prueba cambio de Build-docker.sh

--- .tekton/scripts/build-docker-image.sh
+++ .tekton/scripts/build-docker-image.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-# uncomment to debug the script
-# set -x
-# copy the script below into your app code repo (e.g. ./scripts/build_image_buildkit.sh) and 'source' it from your pipeline job
-#    source ./scripts/build_image_using_buildkit.sh
-# alternatively, you can source it from online script:
-#    source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/build_image_buildkit.sh")
-# ------------------
-# source: https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/build_image_buildkit.sh
-# fail the script if a command fails
-set -eo pipefail
-
-# This script does build a Docker image into IBM Container Service private image registry using Buildkit buildctl - https://github.com/moby/buildkit#building-a-dockerfile-with-buildctl
-# Minting image tag using format: BUILD_NUMBER-BRANCH-COMMIT_ID-TIMESTAMP
-# Also copies information into a build.properties file, so they can be reused later on by other scripts (e.g. image url, chart name, ...)
-
 # Input env variables (can be received via a pipeline environment properties.file.
 echo "REGISTRY_URL=${REGISTRY_URL}"
 echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
@@ -24,111 +8,73 @@ echo "GIT_BRANCH=${GIT_BRANCH}"
 echo "GIT_COMMIT=${GIT_COMMIT}"
 echo "DOCKER_ROOT=${DOCKER_ROOT}"
 echo "DOCKER_FILE=${DOCKER_FILE}"
-
-# View build properties
-if [ -f build.properties ]; then 
-  echo "build.properties:"
-  cat build.properties | grep -v -i password
-else 
-  echo "build.properties : not found"
-fi 
-# also run 'env' command to find all available env variables
-# or learn more about the available environment variables at:
-# https://cloud.ibm.com/docs/services/ContinuousDelivery/pipeline_deploy_var.html#deliverypipeline_environment
-
-# To review or change build options use:
-# ibmcloud cr build --help
-
-# Minting image tag using format: BUILD_NUMBER-BRANCH-COMMIT_ID-TIMESTAMP
-# e.g. 3-master-50da6912-20181123114435
-# (use build number as first segment to allow image tag as a patch release name according to semantic versioning)
-TIMESTAMP=$( date -u "+%Y%m%d%H%M%S")
-IMAGE_TAG=${TIMESTAMP}
-if [ ! -z "${GIT_COMMIT}" ]; then
-  GIT_COMMIT_SHORT=$( echo ${GIT_COMMIT} | head -c 8 ) 
-  IMAGE_TAG=${GIT_COMMIT_SHORT}-${IMAGE_TAG}
-fi
-if [ ! -z "${GIT_BRANCH}" ]; then IMAGE_TAG=${GIT_BRANCH}-${IMAGE_TAG} ; fi
-IMAGE_TAG=${BUILD_NUMBER}-${IMAGE_TAG}
-
-# Checking ig buildctl is installed
-if which buildctl > /dev/null 2>&1; then
-  buildctl --version
-else 
-  echo "Installing Buildkit builctl"
-  curl -sL https://github.com/moby/buildkit/releases/download/v0.8.1/buildkit-v0.8.1.linux-amd64.tar.gz | tar -C /tmp -xz bin/buildctl && mv /tmp/bin/buildctl /usr/bin/buildctl && rmdir --ignore-fail-on-non-empty /tmp/bin
-  buildctl --version
+#Pedazo código nuevo
+if [ $PIPELINE_DEBUG == 1 ]; then
+  pwd
+  env
+  trap env EXIT
+  set -x
 fi
 
-# Create the config.json file to make private container registry accessible
-export DOCKER_CONFIG=$(mktemp -d -t cr-config-XXXXXXXXXX)
-kubectl create secret --dry-run=true --output=json \
-  docker-registry registry-dockerconfig-secret \
-  --docker-server=us.io.cr \
-  --docker-password=${IBMCLOUD_API_KEY} \
-  --docker-username=iamapikey --docker-email=favella@ibm.com | \
-jq -r '.data[".dockerconfigjson"]' | base64 -d > ${DOCKER_CONFIG}/config.json
+source /steps/next-step-env.properties
+export $(cut -d= -f1 /steps/next-step-env.properties)
 
-echo "=========================================================="
-echo -e "BUILDING CONTAINER IMAGE: ${IMAGE_NAME}:${IMAGE_TAG}"
-if [ -z "${DOCKER_ROOT}" ]; then DOCKER_ROOT=. ; fi
-if [ -z "${DOCKER_FILE}" ]; then DOCKER_FILE=Dockerfile ; fi
-if [ -z "$EXTRA_BUILD_ARGS" ]; then
-  echo -e ""
-else
-  for buildArg in $EXTRA_BUILD_ARGS; do
-    if [ "$buildArg" == "--build-arg" ]; then
-      echo -e ""
-    else      
-      BUILD_ARGS="${BUILD_ARGS} --opt build-arg:$buildArg"
-    fi
-  done
+# Manage multiple tags for an image
+# Add dynamically computed tags
+printf "#!/bin/sh\n" > /steps/additionalTags.sh
+printf "%s " '$(params.additional-tags-script)' >> /steps/additionalTags.sh
+chmod +x /steps/additionalTags.sh
+
+# Send stdout to the tags list; don't touch stderr.
+/steps/additionalTags.sh > /steps/tags.lst
+
+# Add image pipeline resource
+if [ "${IMAGE_TAG}" ]; then
+  echo "${IMAGE_TAG}" >> /steps/tags.lst
 fi
-set -x
-buildctl build \
-    --frontend dockerfile.v0 --opt filename=${DOCKER_FILE} --local dockerfile=${DOCKER_ROOT} \
-    ${BUILD_ARGS} --local context=${DOCKER_ROOT} \
-    --import-cache type=registry,ref=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME} \
-    --output type=image,name="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}",push=true
-set +x
-docker buildx create --config /home/rami/workspace/gaia/buildkitd.toml  --name myconfbuilder4
-docker buildx use myconfbuilder4
-
-ibmcloud cr image-inspect ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
-
-# Set PIPELINE_IMAGE_URL for subsequent jobs in stage (e.g. Vulnerability Advisor)
-export PIPELINE_IMAGE_URL="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG"
-
-ibmcloud cr images --restrict ${REGISTRY_NAMESPACE}/${IMAGE_NAME}
-
-######################################################################################
-# Copy any artifacts that will be needed for deployment and testing to $WORKSPACE    #
-######################################################################################
-echo "=========================================================="
-echo "COPYING ARTIFACTS needed for deployment and testing (in particular build.properties)"
-
-echo "Checking archive dir presence"
-if [ -z "${ARCHIVE_DIR}" ]; then
-  echo -e "Build archive directory contains entire working directory."
-else
-  echo -e "Copying working dir into build archive directory: ${ARCHIVE_DIR} "
-  mkdir -p ${ARCHIVE_DIR}
-  find . -mindepth 1 -maxdepth 1 -not -path "./$ARCHIVE_DIR" -exec cp -R '{}' "${ARCHIVE_DIR}/" ';'
+# Add tags provided using task parameter
+if [ "$(params.additional-tags)" ];  then
+  echo "$(params.additional-tags)" | sed 's/,/\n/g' >> /steps/tags.lst
 fi
+echo "#######################"
+echo "Image Tags:"
+cat /steps/tags.lst
+echo "#######################"
+IMAGE_REPOSITORY="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
+# Add the full image url with tags - use # as separator in case IMAGE_NAME contains /
+sed -i "s#^#$IMAGE_REPOSITORY:#" /steps/tags.lst
+sort -u -o /steps/tags.lst /steps/tags.lst
+echo "Full Image URLs:"
+cat /steps/tags.lst
+echo "#######################"
+BUILDKIT_IMAGE_NAMES=$(tr -s '\r\n' ',' < /steps/tags.lst | sed -e 's/,$/\n/')
+if [ -z "$BUILDKIT_IMAGE_NAMES" ]; then
+# Set default image name for buildkit to push
+BUILDKIT_IMAGE_NAMES="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
+fi
+echo "Buildkit Image names: $BUILDKIT_IMAGE_NAMES"
 
-# Persist env variables into a properties file (build.properties) so that all pipeline stages consuming this
-# build as input and configured with an environment properties file valued 'build.properties'
-# will be able to reuse the env variables in their job shell scripts.
+BUILD_ARG_LIST='$(params.build-args)'
+for buildArg in $BUILD_ARG_LIST; do
+BUILD_ARGS="${BUILD_ARGS} --opt build-arg:$buildArg "
+done
 
-# If already defined build.properties from prior build job, append to it.
-cp build.properties $ARCHIVE_DIR/ || :
+buildctl --addr tcp://0.0.0.0:1234 build \
+--progress=plain \
+--frontend=dockerfile.v0 \
+--opt filename=$(params.dockerfile) \
+${BUILD_ARGS} \
+--local context=$(workspaces.source.path)/$(params.path-to-context) \
+--local dockerfile=$(workspaces.source.path)/$(params.path-to-dockerfile) \
+--exporter=image --exporter-opt "name=$BUILDKIT_IMAGE_NAMES" --exporter-opt "push=$(params.push-to-registry)" \
+--export-cache type=inline \
+--import-cache type=registry,ref=$IMAGE_REPOSITORY 2>&1 | tee /steps/build.log
 
-# IMAGE information from build.properties is used in Helm Chart deployment to set the release name
-echo "IMAGE_NAME=${IMAGE_NAME}" >> $ARCHIVE_DIR/build.properties
-echo "IMAGE_TAG=${IMAGE_TAG}" >> $ARCHIVE_DIR/build.properties
-# REGISTRY information from build.properties is used in Helm Chart deployment to generate cluster secret
-echo "REGISTRY_URL=${REGISTRY_URL}" >> $ARCHIVE_DIR/build.properties
-echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}" >> $ARCHIVE_DIR/build.properties
-echo "GIT_BRANCH=${GIT_BRANCH}" >> $ARCHIVE_DIR/build.properties
-echo "File 'build.properties' created for passing env variables to subsequent pipeline jobs:"
-cat $ARCHIVE_DIR/build.properties | grep -v -i password
+# Using the deprecated --exporter option for now as the multiple name/tags using --output option
+# is not working as expected: https://github.com/moby/buildkit/issues/797#issuecomment-581346240
+# --output type=image,"name=$(params.image-url):1.0.0,$(params.image-url)",push=true
+
+# it is not possible to specify multiple exporters for now
+# --output type=oci,dest=/builder/home/image-outputs/built-image/output.tar \
+# It is possible to assign multiple tags to the image with latest version of buildkit-image
+# see https://github.com/moby/buildkit/issues/797
